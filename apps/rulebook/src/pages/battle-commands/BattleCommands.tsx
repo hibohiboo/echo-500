9a60efe4be0efe667c9b8918a0bc10696016aa71
hibohiboo <hibohiboo66+github@gmail.com>
2025-11-16 02:24:07 +0900

    fix 説明 #8

--- apps/rulebook/src/pages/battle-commands/BattleCommands.tsx
+++ apps/rulebook/src/pages/battle-commands/BattleCommands.tsx
@@ -3,6 +3,10 @@ import { BattleCommandCardList } from '@/entities/battle-command';
 function BattleCommands() {
   return (
     <div class="battle-commands-page">
+      <p>戦闘ミッションの前には戦闘のためのモジュールを用意する。</p>
+      <p>
+        戦闘スタイルごとを３つまで選び、選んだスタイルのモジュールをCPを使ってインストールする。
+      </p>
       <BattleCommandCardList />
 
       <style>{`
