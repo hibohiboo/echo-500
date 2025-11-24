b170e0ac7a77f1642b69825eb2b7246f52365974
hibohiboo <hibohiboo66+github@gmail.com>
2025-11-25 01:43:24 +0900

    fix createbrowser router #14

--- apps/rulebook/src/app/router.tsx
+++ apps/rulebook/src/app/router.tsx
@@ -1,5 +1,5 @@
 import { RulebookLayout } from '@echo-500/ui';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { createBrowserRouter } from 'react-router';
 import { BattleCommandsPage } from '@/pages/battle-commands/BattleCommands';
 import { BattleRulesPage } from '@/pages/battle-rules/BattleRules';
 import { SimpleBattleRulePage } from '@/pages/battle-rules/SimpleBattleRulePage';
@@ -12,38 +12,58 @@ import { ScenarioCreatorsPage } from '@/pages/scenario-creators/ScenarioCreators
 import { TermsOfServicePage } from '@/pages/terms-of-service/TermsOfService';
 import { TutorialPage } from '@/pages/tutorial/Tutorial';
 
-export function AppRouter() {
-  const basePath = BASE_PATH;
-
-  return (
-    <BrowserRouter basename={`/${basePath}`}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route element={<RulebookLayout basePath={basePath} />}>
-          <Route path="/content/tutorial" element={<TutorialPage />} />
-          <Route path="/content/glossary" element={<GlossaryPage />} />
-          <Route
-            path="/content/character-creation"
-            element={<CharacterCreationPage />}
-          />
-          <Route
-            path="/content/scenario-creators"
-            element={<ScenarioCreatorsPage />}
-          />
-          <Route path="/content/game-master" element={<GameMasterPage />} />
-          <Route path="/content/battle-rules" element={<BattleRulesPage />} />
-          <Route
-            path="/content/battle-commands"
-            element={<BattleCommandsPage />}
-          />
-          <Route path="/content/privacy" element={<PrivacyPolicyPage />} />
-          <Route path="/content/terms" element={<TermsOfServicePage />} />
-          <Route
-            path="/content/simple-battle-rule"
-            element={<SimpleBattleRulePage />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/content',
+      element: <RulebookLayout basePath={BASE_PATH} />,
+      children: [
+        {
+          path: 'tutorial',
+          element: <TutorialPage />,
+        },
+        {
+          path: 'glossary',
+          element: <GlossaryPage />,
+        },
+        {
+          path: 'character-creation',
+          element: <CharacterCreationPage />,
+        },
+        {
+          path: 'scenario-creators',
+          element: <ScenarioCreatorsPage />,
+        },
+        {
+          path: 'game-master',
+          element: <GameMasterPage />,
+        },
+        {
+          path: 'battle-rules',
+          element: <BattleRulesPage />,
+        },
+        {
+          path: 'battle-commands',
+          element: <BattleCommandsPage />,
+        },
+        {
+          path: 'privacy',
+          element: <PrivacyPolicyPage />,
+        },
+        {
+          path: 'terms',
+          element: <TermsOfServicePage />,
+        },
+        {
+          path: 'simple-battle-rule',
+          element: <SimpleBattleRulePage />,
+        },
+      ],
+    },
+  ],
+  { basename: `/${BASE_PATH}` },
+);
