48373b7c4c9372a1ec3631185210efa03cf20a88
hibohiboo <hibohiboo66+github@gmail.com>
2025-11-30 16:43:34 +0900

    fix loader #16

--- apps/character-sheet/src/pages/character-list/loader.ts
+++ apps/character-sheet/src/pages/character-list/loader.ts
@@ -2,8 +2,6 @@ import { fetchPlayerCharacters } from '@/entities/playerCharacter';
 
 export const createCharacterListLoader =
   (dispatch: AppDispatch) => async () => {
-    console.log('bef');
     const characters = await dispatch(fetchPlayerCharacters());
-    console.log('af', characters);
     return characters;
   };
